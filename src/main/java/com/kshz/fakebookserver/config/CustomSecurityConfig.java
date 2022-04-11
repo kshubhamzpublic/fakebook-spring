61abbacd00ea9db141813e78f37218b26a54e8df
kshubhamzpublic <91407552+kshubhamzpublic@users.noreply.github.com>
2022-04-11 20:15:22 +0530

    Update CustomSecurityConfig.java

--- src/main/java/com/kshz/fakebookserver/config/CustomSecurityConfig.java
+++ src/main/java/com/kshz/fakebookserver/config/CustomSecurityConfig.java
@@ -1,12 +1,16 @@
 package com.kshz.fakebookserver.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 import com.kshz.fakebookserver.jwt.JWTAuthenticationEntryPoint;
 import com.kshz.fakebookserver.jwt.JWTRequestFilter;
@@ -21,9 +25,21 @@ public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JWTRequestFilter jwtRequestFilter;
 	
+	@Value("${cors.origin.allow}")
+	private String allowedOrigins;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		http.cors().configurationSource(request -> {
+			CorsConfiguration cors = new CorsConfiguration();
+			if (allowedOrigins != null) {
+				Arrays.asList(allowedOrigins.split(","))
+					.stream()
+					.forEach(origin -> cors.addAllowedOrigin(origin));
+			}
+			return cors;
+		});
 		
 		http.antMatcher("/**").authorizeRequests() // authorize all rquest
 			.antMatchers("/auth/**", "/post/all").permitAll() // except auth one
