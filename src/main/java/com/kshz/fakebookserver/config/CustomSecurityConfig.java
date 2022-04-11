f019006e31562c8293db67437cc6e29b500db46f
Shubham Kumar <shubhamkumar@Shubhams-MacBook-Air.local>
2022-04-11 20:26:10 +0530

    fixed cors support

--- src/main/java/com/kshz/fakebookserver/config/CustomSecurityConfig.java
+++ src/main/java/com/kshz/fakebookserver/config/CustomSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.kshz.fakebookserver.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -38,6 +39,8 @@ public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
 					.stream()
 					.forEach(origin -> cors.addAllowedOrigin(origin));
 			}
+			cors.setAllowedMethods(List.of("*"));
+			cors.setAllowedHeaders(List.of("*"));
 			return cors;
 		});
 		
